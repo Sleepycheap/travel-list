5c96a66a5dae2602ada675b176f400801bf5e121
Sleepycheap <anthony.vaut@gmail.com>
2026-06-04 17:03:56 -0500

    backup

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import { useState } from "react";
 
 export default function App() {
   const [items, setItems] = useState([]);
-  const [packed, setPacked] = useState(false);
 
   function handleAddItems(item) {
     setItems((items) => [...items, item]);
@@ -29,7 +28,7 @@ export default function App() {
         onDeleteItem={handleDeleteItem}
         onToggleItem={handleTogglePacked}
       />
-      <Stats />
+      <Stats items={items} />
     </div>
   );
 }
@@ -115,10 +114,25 @@ function Item({ item, onDeleteItem, onToggleItem }) {
   );
 }
 
-function Stats() {
+function Stats({ items }) {
+  if (!items.length)
+    return (
+      <p className="stats">
+        <em>Start adding some items to your packing list</em>
+      </p>
+    );
+  const numItems = items.length;
+  const numPacked = items.filter((item) => item.packed).length;
+  const percentage = Math.round((numPacked / numItems) * 100);
+
   return (
     <footer className="stats">
-      <em>You have X items on your list, and you already packed X (X%)</em>
+      <em>
+        {percentage === 100
+          ? "You got everything! ready to go!"
+          : `You have ${numItems} items on your list, and you already packed
+        ${numPacked} (${percentage}%)`}
+      </em>
     </footer>
   );
 }
